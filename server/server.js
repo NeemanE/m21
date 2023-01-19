817227b21907cfed8a2484dc2e88d1c2c83b8792
Neeman Elchurafa <neemanchurafa@gmail.com>
2023-01-18 23:04:46 -0600

    added apollo to server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require('./utils/auth');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
